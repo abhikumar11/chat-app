d65af451a6d90db6360d099d752cfd2125a70cec
abhikumar11 <abhishekkumar12verma@gmail.com>
2023-09-26 19:34:18 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route exact path='/'/>
+      </Routes>
+      <Routes>
+        <Route exact path='/'element={<Login/>}/>
+      </Routes>
     </div>
   );
 }
